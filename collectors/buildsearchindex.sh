523196270c064bd57b566a85945a3f65575d40b7
thexa4 <thexa4@gmail.com>
2017-02-04 20:35:45 +0000

    Use mp3 path instead of git repo path

--- collectors/buildsearchindex.sh
+++ collectors/buildsearchindex.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+mp3path=$(tools/setting Mp3Path)
 file=index.new
-find Artists -iname '*.mp3' > $file
-mv $file index
+find "$mp3path/Artists" -iname '*.mp3' > $file
+mv $file "$mp3path/index"
